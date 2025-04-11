64ce316e4b1fa67a1842a59f0a22a622729f9022
skymwangi <skymwangi526@gmail.com>
2025-04-11 13:15:21 +0300

    commit

--- app/src/main/java/com/online/star2/ui/theme/screens/intent/intentscreen.kt
+++ app/src/main/java/com/online/star2/ui/theme/screens/intent/intentscreen.kt
@@ -1,5 +1,10 @@
 package com.online.star2.ui.theme.screens.intent
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.MediaStore
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,16 +17,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
+
 @Composable
 fun Intent_Screen(navController: NavHostController) {
+    var context= LocalContext.current
     Column (verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
@@ -31,7 +41,23 @@ fun Intent_Screen(navController: NavHostController) {
             color = Color.Red,
             fontFamily = FontFamily.SansSerif,
             fontSize = 30.sp )
-        OutlinedButton(onClick ={/*TODO*/},
+        OutlinedButton(onClick ={
+            val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "+254 750546697"))
+
+            if (ContextCompat.checkSelfPermission(
+                    context,
+                    android.Manifest.permission.CALL_PHONE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    context as Activity,
+                    arrayOf(android.Manifest.permission.CALL_PHONE),
+                    1
+                )
+            } else {
+                context.startActivity(intent)
+            }
+        },
             colors = ButtonDefaults.buttonColors(Color.LightGray),
             modifier = Modifier.width(300.dp)) {
             Text(text = "Call",
@@ -40,7 +66,13 @@ fun Intent_Screen(navController: NavHostController) {
                 color = Color.Black)
 
         }
-        OutlinedButton(onClick ={/*TODO*/},
+        OutlinedButton(onClick ={
+
+            val simToolKitLaunchIntent =
+                context.packageManager.getLaunchIntentForPackage("com.android.stk")
+
+            simToolKitLaunchIntent?.let { context.startActivity(it) }
+        },
             colors = ButtonDefaults.buttonColors(Color.LightGray),
             modifier = Modifier.width(300.dp)) {
             Text(text = "Stk",
@@ -49,7 +81,13 @@ fun Intent_Screen(navController: NavHostController) {
                 color = Color.Black)
 
         }
-        OutlinedButton(onClick ={/*TODO*/},
+        OutlinedButton(onClick ={
+            val phone = "+254 750546697"
+
+            val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null))
+
+            context.startActivity(intent)
+        },
             colors = ButtonDefaults.buttonColors(Color.LightGray),
             modifier = Modifier.width(300.dp)) {
             Text(text = "Dial",
@@ -58,7 +96,17 @@ fun Intent_Screen(navController: NavHostController) {
                 color = Color.Black)
 
         }
-        OutlinedButton(onClick ={/*TODO*/},
+        OutlinedButton(onClick ={
+            val shareIntent = Intent(Intent.ACTION_SEND)
+
+            shareIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+            shareIntent.type = "text/plain"
+
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "Hey, download this app!")
+
+            context.startActivity(shareIntent)
+        },
             colors = ButtonDefaults.buttonColors(Color.LightGray),
             modifier = Modifier.width(300.dp)) {
             Text(text = "Share",
@@ -67,7 +115,15 @@ fun Intent_Screen(navController: NavHostController) {
                 color = Color.Black)
 
         }
-        OutlinedButton(onClick ={/*TODO*/},
+        OutlinedButton(onClick ={
+            val emailIntent=
+                Intent(Intent.ACTION_SENDTO, Uri.fromParts
+                    ("mailto","abcdgmail.com",null))
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT,"Subject")
+
+            emailIntent.putExtra(Intent.EXTRA_TEXT,"Body")
+            context.startActivity(emailIntent)
+        },
             colors = ButtonDefaults.buttonColors(Color.LightGray),
             modifier = Modifier.width(300.dp)) {
             Text(text = "Email",
@@ -76,7 +132,11 @@ fun Intent_Screen(navController: NavHostController) {
                 color = Color.Black)
 
         }
-        OutlinedButton(onClick ={/*TODO*/},
+        OutlinedButton(onClick ={
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            startActivityForResult(context as Activity,takePictureIntent,1,null)
+        },
             colors = ButtonDefaults.buttonColors(Color.LightGray),
             modifier = Modifier.width(300.dp)) {
             Text(text = "Camera",
@@ -85,7 +145,15 @@ fun Intent_Screen(navController: NavHostController) {
                 color = Color.Black)
 
         }
-        OutlinedButton(onClick ={/*TODO*/},
+        OutlinedButton(onClick ={
+            val uri = Uri.parse("smsto:07456789")
+
+            val intent = Intent(Intent.ACTION_SENDTO, uri)
+
+            intent.putExtra("Hello", "How is todays weather")
+
+            context.startActivity(intent)
+        },
             colors = ButtonDefaults.buttonColors(Color.LightGray),
             modifier = Modifier.width(300.dp)) {
             Text(text = "SMS",
--- app/src/main/java/com/online/star2/ui/theme/screens/web/webscreen.kt
+++ app/src/main/java/com/online/star2/ui/theme/screens/web/webscreen.kt
@@ -1,5 +1,7 @@
 package com.online.star2.ui.theme.screens.web
 
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,6 +12,8 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,9 +24,12 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.viewinterop.AndroidView
 
 @Composable
 fun Web_Screen(navController: NavHostController) {
+    var url by remember { mutableStateOf("") }
     Column (verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
@@ -33,13 +40,23 @@ fun Web_Screen(navController: NavHostController) {
             fontFamily = FontFamily.Cursive,
             fontSize = 30.sp
         )
-        OutlinedButton(onClick ={/*TODO*/},
+        OutlinedButton(onClick ={
+            url= "https://www.google.com/"
+        },
             colors = ButtonDefaults.buttonColors(Color.Unspecified),
             modifier = Modifier.width(300.dp)) {
             Text(text = "Internet",
                 fontSize = 35.sp,
                 fontFamily = FontFamily.Serif)
 
+        }
+        if (url.isEmpty()){
+            AndroidView(factory = {context ->
+                WebView(context).apply {
+                    webViewClient= WebViewClient()
+                    settings.javaScriptEnabled=true
+                    loadUrl(url)
+                }})
         }
     }
 
